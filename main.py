d2320626340200aea42612e11d8a86a55e3272a2
NeiraJ <johnny.neira@gmail.com>
2024-07-18 11:23:22 +0200

    feat: ajouter les fonctions ask_players et get_and_switch_player_response
    - La fonction ask_players demande à un joueur combien d'allumettes il souhaite enlever.
    - La fonction get_and_switch_player_response retourne la réponse du joueur actuel en utilisant ask_players.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 def game_title(func: any):
     """
     Décorateur qui efface la console et imprime un titre de jeu avant d'exécuter la fonction décorée.
-    Args:
-        func (any):
+    Args :
+        func (any) :
     """
 
     def wrapper(*args, **kwargs):
@@ -39,30 +39,63 @@ def get_player_names():
 
 
 @game_title
-def determine_the_starting_player(players: tuple) -> str:
-    """    
-    Demande à l'utilisateur de choisir le joueur qui commence le jeu entre deux joueurs spécifiés.
+def determine_the_starting_player(players: tuple) -> int:
     """
-    return input(f"Qui Commence ? {players[0]} (1) ou {players[1]} (2)? : ")
+    Détermine le joueur qui commence en demandant à l'utilisateur.
+
+    :param players: (tuple): Noms des joueurs.
+    :return: (int): Indice du joueur débutant.
+    """
+
+    return int(input(f"Qui Commence ? {players[0]} (1) ou {players[1]} (2)? : ")) - 1
 
 
 def game_initialization() -> list:
     """
-    La fonction game_initialization crée une liste de 21 éléments, chacun  à la lettre 'l'.
+    La fonction game_initialization crée une liste de 21 éléments, chacun à la lettre 'l'.
     """
     return ['l' for _ in range(21)]
 
 
 def removing_elements(elements: list, number_of_elements_to_remove: int) -> None:
+    """
+    Supprime un nombre spécifié d'éléments 'l' d'une liste donnée.
+    """
     for _ in range(number_of_elements_to_remove):
         elements.remove('l')
 
 
+@game_title
+def ask_players(player: str) -> int:
+    """
+    Demande à un joueur combien d'allumettes il souhaite enlever.
+    """
+    return int(input(f" {player}, combien d'allumettes souhaitez-vous enlever? :"))
+
+
+def get_and_switch_player_response(player_index, player_names):
+    """
+    retourne la réponse du joueur actuel.
+    :param player_index: (int): Indice du joueur actuel.
+    :param player_names: (list): Liste des noms des joueurs.
+    :return: (int): La réponse du joueur actuel.
+    """
+    player_response: int = ask_players(player_names[player_index])
+
+    return player_response
+
+
 def main():
-    player_names = get_player_names()
-    determine_the_starting_player(player_names)
+    player_names: tuple[str, str] = get_player_names()
+    starting_player: int = determine_the_starting_player(player_names)
+    list_of_elements: list = game_initialization()
+
+    player_index = player_names.index(player_names[starting_player])
+
+    while len(list_of_elements) != 0:
+        player_response: int = ask_players(player_names[player_index])
+        player_index ^= 1
 
 
 if __name__ == "__main__":
-    # main()
-    print(game_initialization())
+    main()
